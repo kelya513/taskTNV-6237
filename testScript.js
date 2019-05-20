bd9a2078ac82273f6927b05dd972eb81e80fd411
Anna Kirienko <anurcha11@gmail.com>
2019-05-20 16:36:44 +0300

    Email added

--- testScript.js
+++ testScript.js
@@ -1,7 +1,13 @@
 let name = "Hanna";
+
 let surName = "Kiryienka";
+
 console.log(name, surName);
 
 alert("Let's have some fun with git");
 
 alert("Switched to branch test!");
+
+let email = "anurcha11@gmail.com";
+
+console.log(email);
